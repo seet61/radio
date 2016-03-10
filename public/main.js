59aa51878aba917b0c52a608b80f3562446becdc
seet61 <seet61@gmail.com>
2016-03-10 19:04:11 +0300

    удалил старую версию

--- public/main.js
+++
@@ -1,5 +0,0 @@
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Привет, world!'
-), document.getElementById('example'));
--- src/main.js
+++
@@ -1,4 +0,0 @@
-ReactDOM.render(
-  <h1>Привет, world!</h1>,
-  document.getElementById('example')
-);
